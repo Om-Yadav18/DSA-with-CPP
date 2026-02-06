6807995b99976d53f37aec6787f81a6389bde773
Om Yadav <omyadav9896@gmail.com>
2026-02-06 23:37:52 +0530

    Update TwoSum.cpp

---
+++ Arrays/TwoSum.cpp
@@ -0,0 +1,22 @@
+// Problem: Two Sum
+// Approach: Hash Map
+// Time: O(n), Space: O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    unordered_map<int, int> mp;
+    for (int i = 0; i < nums.size(); i++) {
+        int need = target - nums[i];
+        if (mp.count(need)) {
+            cout << mp[need] << " " << i;
+            return 0;
+        }
+        mp[nums[i]] = i;
+    }
+    return 0;
+}
